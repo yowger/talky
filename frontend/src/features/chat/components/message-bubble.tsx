f16d19190c4638cd35d94968bd4f695eec22c2e3
Roger Pantil <rogerpantilyowger@gmail.com>
2024-09-30 22:35:01 +0800

    updated message bubble styles

--- frontend/src/features/chat/components/message-bubble.tsx
+++ frontend/src/features/chat/components/message-bubble.tsx
@@ -1,4 +1,10 @@
-import { format, formatDistanceToNow, isToday, isThisWeek } from "date-fns"
+import {
+    format,
+    formatDistanceToNow,
+    isToday,
+    isThisWeek,
+    isThisYear,
+} from "date-fns"
 
 import { cn } from "@/lib/utils"
 
@@ -24,48 +30,86 @@ export default function MessageBubble(props: MessageBubbleProps) {
                 isCurrentUser ? "justify-end" : "justify-start"
             )}
         >
-            {!isCurrentUser && (
+            <div
+                className={cn(
+                    "flex gap-1  max-w-[56ch]",
+                    isCurrentUser && "flex-row-reverse"
+                )}
+            >
                 <AvatarWithStatus
                     src={message.sender.avatar}
                     name={message.sender.name}
                 />
-            )}
-            <div className="flex flex-col gap-1 w-full max-w-[320px]">
-                <div
-                    className={cn(
-                        "flex items-center space-x-2 rtl:space-x-reverse",
-                        isCurrentUser ? "justify-end" : "justify-start"
-                    )}
-                >
-                    {!isCurrentUser && (
-                        <span className="text-sm font-semibold text-gray-900 dark:text-white">
-                            {message.sender.name}
-                        </span>
-                    )}
-                    <span className="text-xs font-normal text-gray-500 dark:text-gray-400">
-                        {formattedTimestamp}
-                    </span>
-                </div>
-                <div
-                    className={cn(
-                        "flex flex-col leading-1.5 p-4 border-gray-200",
-                        isCurrentUser
-                            ? "rounded-s-xl rounded-ee-xl bg-blue-500"
-                            : "rounded-e-xl rounded-es-xl bg-gray-100  dark:bg-gray-700"
-                    )}
-                >
-                    <p
-                        className={cn(
-                            "text-sm font-normal text-gray-900 dark:text-white",
-                            isCurrentUser
-                                ? "text-white"
-                                : "text-gray-900 dark:text-white"
-                        )}
-                    >
-                        {message.content}
-                    </p>
+                <div className="flex flex-col gap-1 w-full">
+                    <MessageContent
+                        isCurrentUser={isCurrentUser}
+                        content={message.content}
+                    />
+                    <MessageHeader
+                        isCurrentUser={isCurrentUser}
+                        senderName={message.sender.name}
+                        date={formattedTimestamp}
+                    />
                 </div>
             </div>
+        </div>
+    )
+}
+
+interface MessageHeaderProps {
+    isCurrentUser: boolean
+    senderName: string
+    date: string
+}
+
+function MessageHeader({
+    isCurrentUser,
+    senderName,
+    date,
+}: MessageHeaderProps) {
+    return (
+        <div
+            className={cn(
+                "flex items-center gap-2",
+                isCurrentUser ? "flex-row-reverse" : ""
+            )}
+        >
+            <span className="text-xs text-gray-500 dark:text-white">
+                {senderName}
+            </span>
+            <span className="text-sm text-gray-500 dark:text-white">•</span>
+            <span className="text-xs font-normal text-gray-500 dark:text-gray-400">
+                {date}
+            </span>
+        </div>
+    )
+}
+
+interface MessageContentProps {
+    isCurrentUser: boolean
+    content: string
+}
+
+function MessageContent({ isCurrentUser, content }: MessageContentProps) {
+    return (
+        <div
+            className={cn(
+                "flex flex-col leading-1.5 p-4 border-gray-200",
+                isCurrentUser
+                    ? "rounded-s-xl rounded-ee-xl bg-blue-500"
+                    : "rounded-e-xl rounded-es-xl bg-gray-100 dark:bg-gray-700"
+            )}
+        >
+            <p
+                className={cn(
+                    "text-sm font-normal",
+                    isCurrentUser
+                        ? "text-white"
+                        : "text-gray-900 dark:text-white"
+                )}
+            >
+                {content}
+            </p>
         </div>
     )
 }
@@ -79,5 +123,9 @@ function formatMessageTimestamp(date: Date): string {
         return format(date, "EEEE, hh:mm a")
     }
 
-    return format(date, "MMM, dd yyyy hh:mm a")
+    if (isThisYear(date)) {
+        return format(date, "MMM dd, hh:mm a")
+    }
+
+    return format(date, "MMM dd yyyy, hh:mm a")
 }
