b86f2e47e64b278c186044a7bcdc04e34b4d6d4f
Roger Pantil <rogerpantilyowger@gmail.com>
2024-10-07 00:06:44 +0800

    updated logger config

--- backend/src/config/logger.ts
+++ backend/src/config/logger.ts
@@ -1,12 +1,20 @@
 import pino from "pino"
+import { config } from "./config"
+
+const level = config.nodeEnv === "development" ? "debug" : "info"
 
 const logger = pino({
     transport: {
-        target: "pino-pretty",
+        level,
+        target: "pino/file",
         options: {
             colorize: true,
+            destination: "./logs/app.log",
+            mkdir: true,
         },
     },
 })
 
 export default logger
+
+// future todo - add redaction for sensitive data
