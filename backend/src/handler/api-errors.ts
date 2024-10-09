c640c96b5c11c16c7c86fd3e6f05b14b5f85dc3f
Roger Pantil <rogerpantilyowger@gmail.com>
2024-10-09 21:04:19 +0800

    updated api error class, change name, added additional info

--- backend/src/handler/api-errors.ts
+++ backend/src/handler/api-errors.ts
@@ -8,99 +8,123 @@ export enum HttpStatusCodes {
     INTERNAL_SERVER_ERROR = 500,
 }
 
+type Details = Record<string, any>
+
 interface ErrorConfig {
     name: string
     httpStatusCode: HttpStatusCodes
     isOperational?: boolean
     description?: string
+    details?: Details
 }
 
-export class BaseError extends Error {
+export class ApiError extends Error {
     public readonly httpStatusCode: HttpStatusCodes
     public readonly isOperational: boolean
+    public readonly details: Details
 
-    constructor({
-        name,
-        httpStatusCode,
-        isOperational = true,
-        description,
-    }: ErrorConfig) {
-        super(description || name)
+    constructor(errorConfig: ErrorConfig) {
+        const {
+            name,
+            httpStatusCode,
+            isOperational = true,
+            description,
+            details,
+        } = errorConfig
+
+        super(description)
 
         Object.setPrototypeOf(this, new.target.prototype)
 
         this.name = name
         this.httpStatusCode = httpStatusCode
         this.isOperational = isOperational
+        this.details = details || {}
 
         Error.captureStackTrace(this)
     }
 }
 
-export class BadRequestError extends BaseError {
+export class BadRequestError extends ApiError {
     constructor(
-        description = "The request cannot be fulfilled due to bad syntax."
+        description = "The request cannot be fulfilled due to bad syntax.",
+        details: Details = {}
     ) {
         super({
             name: "Bad Request",
             httpStatusCode: HttpStatusCodes.BAD_REQUEST,
             description,
+            details,
         })
     }
 }
 
-export class UnauthorizedError extends BaseError {
-    constructor(description = "You are not authorized.") {
+export class UnauthorizedError extends ApiError {
+    constructor(
+        description = "You are not authorized.",
+        details: Details = {}
+    ) {
         super({
             name: "Unauthorized",
             httpStatusCode: HttpStatusCodes.UNAUTHORIZED,
             description,
+            details,
         })
     }
 }
 
-export class ForbiddenError extends BaseError {
+export class ForbiddenError extends ApiError {
     constructor(
-        description = "You don't have permission to access the requested resource."
+        description = "You don't have permission to access the requested resource.",
+        details: Details = {}
     ) {
         super({
             name: "Forbidden",
             httpStatusCode: HttpStatusCodes.FORBIDDEN,
             description,
+            details,
         })
     }
 }
 
-export class NotFoundError extends BaseError {
-    constructor(description = "The requested resource could not be found.") {
+export class NotFoundError extends ApiError {
+    constructor(
+        description = "The requested resource could not be found.",
+        details: Details = {}
+    ) {
         super({
             name: "Not Found",
             httpStatusCode: HttpStatusCodes.NOT_FOUND,
             description,
+            details,
         })
     }
 }
 
-export class ConflictError extends BaseError {
+export class ConflictError extends ApiError {
     constructor(
-        description = "The request could not be completed because of a conflict in the request."
+        description = "The request could not be completed because of a conflict in the request.",
+        details: Details = {}
     ) {
         super({
             name: "Conflict",
             httpStatusCode: HttpStatusCodes.CONFLICT,
             description,
+            details,
         })
     }
 }
 
-export class InternalServerError extends BaseError {
+export class InternalServerError extends ApiError {
     constructor(
-        description = "Internal Server Error. Please try again later."
+        description = "Internal Server Error. Please try again later.",
+        details: Details = {}
     ) {
         super({
             name: "Internal Server Error",
             httpStatusCode: HttpStatusCodes.INTERNAL_SERVER_ERROR,
             description,
+            details,
         })
     }
 }
