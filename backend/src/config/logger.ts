6d5af1cd8198c96c64e9f3f757af3804b015e103
Roger Pantil <rogerpantilyowger@gmail.com>
2024-10-09 21:32:27 +0800

    refactored logger

--- backend/src/config/logger.ts
+++ backend/src/config/logger.ts
@@ -1,26 +1,33 @@
 import pino from "pino"
+
 import { config } from "./config"
+
+import type { TransportTargetOptions } from "pino"
 
 const isDevelopment = config.nodeEnv === "development"
 
 const level = isDevelopment ? "debug" : "info"
 
-const transport = isDevelopment
-    ? {
-          level,
-          target: "pino-pretty",
-          options: {
-              colorize: true,
-          },
-      }
-    : {
-          level,
-          target: "pino/file",
-          options: {
-              destination: "./logs/app.log",
-              mkdir: true,
-          },
-      }
+const devTransport: TransportTargetOptions = {
+    level,
+    target: "pino-pretty",
+    options: {
+        colorize: true,
+    },
+}
+
+const prodTransport: TransportTargetOptions = {
+    level,
+    target: "pino/file",
+    options: {
+        destination: "./logs/app.log",
+        mkdir: true,
+    },
+}
+
+const transport: TransportTargetOptions = isDevelopment
+    ? devTransport
+    : prodTransport
 
 const logger = pino({ transport })
 
