c6a4e34bffbe69bf576e1e4d78e9725f0ced71f9
Roger Pantil <rogerpantilyowger@gmail.com>
2024-10-23 00:09:38 +0800

    added ts magic explanation in case forget

--- backend/src/types/utils-types.ts
+++ backend/src/types/utils-types.ts
@@ -1,6 +1,8 @@
 // typescript magic
 
 export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>
+// picked property can be optional
+// type OptionalEmailUser = Optional<User, 'email'>;
 
 type Only<T, U> = {
     [P in keyof T]: T[P]
@@ -8,3 +10,5 @@ type Only<T, U> = {
     [P in keyof U]?: never
 }
 export type Either<T, U> = Only<T, U> | Only<U, T>
+// either of the two types
+// type UserOrAdmin = Either<User, Admin>
